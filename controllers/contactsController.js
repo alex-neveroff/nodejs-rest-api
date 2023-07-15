37e818221ec64f0d8f32b263a61a4e599a6d2942
Alexandr Neveroff <neveroff1976@gmail.com>
2023-07-16 01:27:06 +0300

    Decorators added

---
+++ controllers/contactsController.js
@@ -0,0 +1,48 @@
+import contactsService from "../models/contacts.js";
+import { HttpError } from "../helpers/index.js";
+import { controllerWrapper } from "../decorators/index.js";
+
+const getAll = async (req, res) => {
+  const result = await contactsService.listContacts();
+  res.json(result);
+};
+
+const getById = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await contactsService.getContactById(contactId);
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.json(result);
+};
+
+const add = async (req, res) => {
+  const result = await contactsService.addContact(req.body);
+  res.status(201).json(result);
+};
+
+const updateById = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await contactsService.updateContact(contactId, req.body);
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.json(result);
+};
+
+const deleteById = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await contactsService.removeContact(contactId);
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.json({ message: "contact deleted" });
+};
+
+export default {
+  getAll: controllerWrapper(getAll),
+  getById: controllerWrapper(getById),
+  add: controllerWrapper(add),
+  updateById: controllerWrapper(updateById),
+  deleteById: controllerWrapper(deleteById),
+};
--- helpers/index.js
+++ helpers/index.js
@@ -1,1 +1,3 @@
 export { default as HttpError } from "./HttpError.js";
+export { default as isEmptyBody } from "./isEmptyBody.js";
+export { default as contactAddSchema } from "./schemas.js";
