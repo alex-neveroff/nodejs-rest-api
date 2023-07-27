3519bb3b0009111f796d33134ef6166bbf58acde
Alexandr Neveroff <neveroff1976@gmail.com>
2023-07-27 14:24:16 +0300

    api/users => users

--- controllers/authController.js
+++ controllers/authController.js
@@ -60,7 +60,7 @@ const getCurrent = async (req, res) => {
 const logout = async (req, res) => {
   const { _id } = req.user;
   await User.findByIdAndUpdate(_id, { token: "" });
-  res.status(204);
+  res.status(204).end();
 };
 
 const updateUserSubscription = async (req, res) => {
